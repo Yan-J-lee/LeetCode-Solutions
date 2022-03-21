c73664d682f62bc9aa6429ed071ff751091233ef
Geek <71831411+Yan-J-lee@users.noreply.github.com>
2022-03-21 18:39:19 +0800

    Inverting BinTree

--- InvertBinTree_226.cpp
+++ InvertBinTree_226.cpp
@@ -17,3 +17,13 @@ public:
         return root;
     }
 };
+
+# class Solution(object):
+#    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+#        if root:
+#            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+#        return root
