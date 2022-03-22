fbd183382baeec26e68ba95238d94db745925744
Geek <71831411+Yan-J-lee@users.noreply.github.com>
2022-03-22 14:45:10 +0800

    Diameter of BinTree

--- DiameterOfBinTree.cpp
+++ DiameterOfBinTree.cpp
@@ -6,39 +6,23 @@ Algorithm:
 */
 
 class Solution {
-    private:
-        int diameter = 0;
-    public:
-        int getDepth(TreeNode *tree) {
-            // base case
-            if (tree == NULL) {
-                return 0;
-            }
-            else {
-                int LD = getDepth(tree -> left);
-                int RD = getDepth(tree -> right);
-                return (LD>RD?LD:RD) + 1;
-            }
+private:
+    int diameter = 0;
+public:
+    int getDepth(TreeNode *tree) {
+        int LD, RD;
+        // base case
+        if (tree == NULL) {
+            return 0;
         }
-        
-        void getDiameter(TreeNode *tree) {
-            if (tree == NULL) {
-                return 0;
-            }
-            else
-            {
-                int LeftDepth = getDepth(tree -> left);
-                int RightDepth = getDepth(tree -> right);
-                int temp = LeftDepth + RightDepth;
-                diameter = temp > diameter ? temp : diameter;
-                
-                getDiameter(tree -> left);
-                getDiameter(tree -> right);
-            }
-        }
-        
-        int diameterOfBinaryTree(TreeNode *root) {
-            getDiameter(root);
-            return diameter;
-        }
+        LD = getDepth(tree -> left);
+        RD = getDepth(tree -> right);
+        diameter = diameter > (LD+RD) ? diameter : (LD+RD);
+        return (LD>RD?LD:RD) + 1;
+    }
+ 
+    int diameterOfBinaryTree(TreeNode* root) {
+        getDepth(root);
+        return diameter;
+    }
 };
