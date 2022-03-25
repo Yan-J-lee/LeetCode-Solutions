b82bae796a1fa11a4149ba3b00efac4b17fa3b41
Geek <71831411+Yan-J-lee@users.noreply.github.com>
2022-03-25 11:12:27 +0800

    Build Maximum BinTree

--- MaxBinTree_654.cpp
+++ MaxBinTree_654.cpp
@@ -3,7 +3,7 @@
 2. construct the left subtree based on the root recursively
 3. build the right subtree based on the root recursively
 */
-
+// C++
 class Solution {
     TreeNode *maximumBinaryTree(vector<int> nums, int low, int high) {
         // base case
@@ -30,3 +30,19 @@ class Solution {
         return maximumBinaryTree(nums, 0, nums.size()-1);
     }
 };
+
+// Python
+# class Solution(object):
+#    def constructMaximumBinaryTree(self, nums):
+#        """
+#        :type nums: List[int]
+#        :rtype: TreeNode
+#        """
+#        if nums:
+#            max_idx, max_val = max(enumerate(nums), key=lambda x:x[1])
+#            root = TreeNode(val=max_val)
+#            root.left = self.constructMaximumBinaryTree(nums[:max_idx])
+#            root.right = self.constructMaximumBinaryTree(nums[max_idx+1:])
+#            return root
+#        else:
+#            return None
