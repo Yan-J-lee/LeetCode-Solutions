b6313ef7fc318a099811b2c5f01b2f60df765144
Geek <71831411+Yan-J-lee@users.noreply.github.com>
2022-03-28 11:05:13 +0800

    Two Pointers

---
+++ MiddleOfLinkedList_876.cpp
@@ -0,0 +1,13 @@
+// Two Pointers
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast != NULL && fast -> next != NULL) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        return slow;
+    }
+};
