1cfc07ca224c53db7aaaf5c179035ab4f332a6d3
Geek <71831411+Yan-J-lee@users.noreply.github.com>
2022-04-05 22:34:15 +0800

    Fibonacci Array

--- FibonacciNumber_509.cpp
+++ FibonacciNumber_509.cpp
@@ -25,3 +25,25 @@ public:
         return dp[n];
     }
 };
+
+// improved DP (avoid DP Table; reduce S(n) from O(n) to O(1))
+class Solution {
+public:
+    int fib(int n) {
+        // base case
+        if (n == 0 || n == 1)
+            return n;
+        
+        int dp_i_1 = 1;  // dp[i-1]
+        int dp_i_2 = 0;  // dp[i-2]
+        
+        for (int i = 2; i <= n; ++i) {
+            // state transition
+            int dp_i = dp_i_1 + dp_i_2;  // dp[i] = dp[i-1] + dp[i-2]
+            dp_i_2 = dp_i_1;  // dp[i-2] = dp[i-1]
+            dp_i_1 = dp_i;  // dp[i-1] = dp[i]
+        }
+        
+        return dp_i_1;
+    }
+};
